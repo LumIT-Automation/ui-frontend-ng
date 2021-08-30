0843906cc94f0f8d69e18eedf39dd2f4aa74f090
Lorenzo <l.pellegrin@lumit.it>
2021-08-30 19:01:46 +0200

    url to backend commented

--- src/config.js
+++ src/config.js
@@ -1,9 +1,12 @@
 const CONFIG = {
     AUTH_URL: "/auth/api/v1/token/",
+    //AUTH_URL: "http://10.0.111.100/api/v1/token/", // use in DEVELOPMENT WHEN NOT USING REVP - line will be automatically removed in production's code.
 
     SUPERADMIN_URL: "/auth/api/v1/superadmins/",
+    //SUPERADMIN_URL: "http://10.0.111.100/api/v1/token/superadmins/", // use in DEVELOPMENT WHEN NOT USING REVP - line will be automatically removed in production's code.
 
     BACKEND_URL: "/backend/"
+    //BACKEND_URL: "http://10.0.111.12/backend/f5/" // use in DEVELOPMENT WHEN NOT USING REVP - line will be automatically removed in production's code.
 }
 
 export default CONFIG;
