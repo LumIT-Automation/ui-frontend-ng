4bc6189c7737f80b5ec4f50b735c2890b22e7944
Lorenzo <l.pellegrin@lumit.it>
2022-03-02 14:56:08 +0100

    fastweb commented

--- src/concerto/homepage.js
+++ src/concerto/homepage.js
@@ -70,84 +70,78 @@ class Homepage extends React.Component {
   render() {
     return (
       <React.Fragment>
-
-        <br/>
-        <Divider orientation="left">Devices</Divider>
-
+      {/*
         <React.Fragment>
-          <Row>
-            <Col span={17}>
-              <Row>
-                <Col span={6}>
-                  <Card title={<p style={{textAlign: 'center'}}>FIRMWARE</p>} bordered={true}>
-                    <Firmware/>
-                  </Card>
-                </Col>
-                <Col span={6}>
-                  <Card title={<p style={{textAlign: 'center'}}>FIRMWARE0</p>} bordered={true}>
-                    <Firmware0/>
-                  </Card>
-                </Col>
-                {/*
+          <br/>
+          <Divider orientation="left">Devices</Divider>
+          <React.Fragment>
+            <Row>
+              <Col span={17}>
+                <Row>
+                  <Col span={6}>
+                    <Card title={<p style={{textAlign: 'center'}}>FIRMWARE</p>} bordered={true}>
+                      <Firmware/>
+                    </Card>
+                  </Col>
+                  <Col span={6}>
+                    <Card title={<p style={{textAlign: 'center'}}>FIRMWARE0</p>} bordered={true}>
+                      <Firmware0/>
+                    </Card>
+                  </Col>
                   <Col span={6}>
                     <Card title={<p style={{textAlign: 'center'}}>MODELLO</p>} bordered={true}>
                       <Modello/>
                     </Card>
                   </Col>
-                */}
+                  <Col span={6}>
+                    <Card title={<p style={{textAlign: 'center'}}>BACKUP STATUS</p>} bordered={true}>
+                      <BackupStatus/>
+                    </Card>
+                  </Col>
+                  <Col span={6}>
+                    <Card title={<p style={{textAlign: 'center'}}>ATTIVAZIONE</p>} bordered={true}>
+                      <Attivazione/>
+                    </Card>
+                  </Col>
+                  <Col span={6}>
+                    <Card title={<p style={{textAlign: 'center'}}>EOS FIRMWARE</p>} bordered={true}>
+                      <EosFirmware/>
+                    </Card>
+                  </Col>
+                  <Col span={6}>
+                    <Card title={<p style={{textAlign: 'center'}}>EOS HARDWARE</p>} bordered={true}>
+                      <EosHardware/>
+                    </Card>
+                  </Col>
+                </Row>
+              </Col>
 
-                <Col span={6}>
-                  <Card title={<p style={{textAlign: 'center'}}>BACKUP STATUS</p>} bordered={true}>
-                    <BackupStatus/>
-                  </Card>
-                </Col>
-                <Col span={6}>
-                  <Card title={<p style={{textAlign: 'center'}}>ATTIVAZIONE</p>} bordered={true}>
-                    <Attivazione/>
-                  </Card>
-                </Col>
+              <Col offset={0} span={7}>
+                <Card title={<p style={{textAlign: 'center'}}>REGION</p>} bordered={true}>
+                  <Italia />
+                </Card>
+              </Col>
+            </Row>
+          </React.Fragment>
 
-              </Row>
-              <Row>
+          <br/>
+          <Divider orientation="left">Projects</Divider>
+          <React.Fragment>
+            <Row>
               <Col span={6}>
-                <Card title={<p style={{textAlign: 'center'}}>EOS FIRMWARE</p>} bordered={true}>
-                  <EosFirmware/>
+                <Card title={<p style={{textAlign: 'center'}}>SERVIZIO</p>} bordered={true}>
+                  <Servizio/>
                 </Card>
               </Col>
               <Col span={6}>
-                <Card title={<p style={{textAlign: 'center'}}>EOS HARDWARE</p>} bordered={true}>
-                  <EosHardware/>
+                <Card title={<p style={{textAlign: 'center'}}>SEGMENTO</p>} bordered={true}>
+                  <Segmento/>
                 </Card>
               </Col>
-
-              </Row>
-            </Col>
-
-            <Col offset={0} span={7}>
-              <Card title={<p style={{textAlign: 'center'}}>REGION</p>} bordered={true}>
-                <Italia />
-              </Card>
-            </Col>
-          </Row>
+            </Row>
+          </React.Fragment>
         </React.Fragment>
-
-        <br/>
-        <Divider orientation="left">Projects</Divider>
-        <React.Fragment>
-          <Row>
-            <Col span={6}>
-              <Card title={<p style={{textAlign: 'center'}}>SERVIZIO</p>} bordered={true}>
-                <Servizio/>
-              </Card>
-            </Col>
-            <Col span={6}>
-              <Card title={<p style={{textAlign: 'center'}}>SEGMENTO</p>} bordered={true}>
-                <Segmento/>
-              </Card>
-            </Col>
-          </Row>
-        </React.Fragment>
-
+      */}
       </React.Fragment>
     )
   }
