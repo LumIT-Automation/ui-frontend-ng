e1fd8e975c27437c4fbeb29b363eda0eb4f722ac
Lorenzo <l.pellegrin@lumit.it>
2022-03-09 13:28:56 +0100

    hide homepage

--- src/concerto/homepage.js
+++ src/concerto/homepage.js
@@ -47,7 +47,7 @@ class Homepage extends React.Component {
   render() {
     return (
       <React.Fragment>
-
+      {/*
         <React.Fragment>
           <br/>
           <Divider orientation="left">Devices</Divider>
@@ -118,7 +118,7 @@ class Homepage extends React.Component {
             </Row>
           </React.Fragment>
         </React.Fragment>
-
+      */}
       </React.Fragment>
     )
   }
