45a87c210c9bafd750c835a8376d8ff6d90fb885
Lorenzo <l.pellegrin@lumit.it>
2022-11-10 15:23:21 +0100

    fix

--- src/f5/nodes/manager.js
+++ src/f5/nodes/manager.js
@@ -42,7 +42,7 @@ class Manager extends React.Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if ( (this.props.asset && this.props.partition && !this.props.nodesError) ) {
+    if ( (this.props.asset && this.props.partition && !prevProps.nodesError && !this.props.nodesError) ) {
       if (!this.props.nodes) {
         this.nodesGet()
       }
