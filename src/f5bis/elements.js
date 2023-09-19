387e26f9f1a9f5491c55ae9864ac22457368b7f4
Lorenzo <l.pellegrin@lumit.it>
2023-09-19 19:33:19 +0200

    f5 node refactor

--- src/f5bis/elements.js
+++ src/f5bis/elements.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import 'antd/dist/antd.css';
 import '../App.css'
-import { Space, Table, Input, Button, Spin, Checkbox, Radio } from 'antd';
+import { Space, Table, Input, Select, Button, Spin, Checkbox, Radio } from 'antd';
 import Highlighter from 'react-highlight-words';
 import { SearchOutlined, LoadingOutlined, ReloadOutlined } from '@ant-design/icons';
 
@@ -12,28 +12,45 @@ import CommonFunctions from '../_helpers/commonFunctions';
 
 import {
   nodesError,
+  routeDomainsError,
 } from './store'
   
 const spinIcon = <LoadingOutlined style={{ fontSize: 50 }} spin />
 const elementLoadIcon = <LoadingOutlined style={{ fontSize: 25 }} spin />
 
+/*
+@todo:
+aggiornamento antd
+foreach --> map
+1 funzione, 1 azione
+1 get
+1 set
+validation
+del, post, patch
+createElement
+localStorage
+*/ 
 
 class F5Elements extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.myRefs = {};
+
     this.state = {
       searchText: '',
       searchedColumn: '',
       f5elements: [],
       originf5elements: [],
       element: 'node',
+      nodeSessions: ['user-enabled', 'user-disabled'],
+      nodeStates: ['unchecked', 'user-down'],
       errors: {}
     };
   }
 
   componentDidMount() {
-    console.log('mount')
     if (this.props.f5elements) {
       this.main()
     }
@@ -44,7 +61,10 @@ class F5Elements extends React.Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (this.props.f5elements !== prevProps.f5elements) {
+    console.log(this.state.routeDomains)
+    console.log(this.state.f5elements)
+
+    if (this.props.f5elements !== prevProps.f5elements || this.props.asset !== prevProps.asset || this.props.partition !== prevProps.partition) {
       this.main()
     }
   }
@@ -135,8 +155,18 @@ class F5Elements extends React.Component {
 
   main = async () => {
     await this.setState({loading: true})
+
+    let routeDomains = await this.dataGet('routedomains', this.props.asset.id)
+    if (routeDomains.status && routeDomains.status !== 200 ) {
+      this.props.dispatch(routeDomainsError(routeDomains))
+      await this.setState({loading: false})
+      return
+    }
+    else {
+      await this.setState({routeDomains: routeDomains.data.items})
+    }
+
     let f5elements = await this.dataGet(this.props.f5elements, this.props.asset.id)
-    console.log(f5elements)
     let id = 1
     
     if (f5elements.status && f5elements.status !== 200 ) {
@@ -162,6 +192,9 @@ class F5Elements extends React.Component {
     if (assetId) {
       endpoint = `f5/${assetId}/${this.props.partition}/${entities}/`
     }
+    if (entities === 'routedomains') {
+      endpoint = `f5/${assetId}/${entities}/`
+    }
     let rest = new Rest(
       "GET",
       resp => {
@@ -182,8 +215,6 @@ class F5Elements extends React.Component {
   }
 
   elementRemove = async (el, elements) => {
-    console.log('el', el)
-    console.log('elements', elements)
     let commonFunctions = new CommonFunctions()
     let list = await commonFunctions.elementRemove(el, elements)
     await this.setState({f5elements: list})
@@ -192,62 +223,297 @@ class F5Elements extends React.Component {
   set = async (key, value, el) => {
     let elements = JSON.parse(JSON.stringify(this.state.f5elements))
     let origEl = this.state.originf5elements.find(e => e.id === el.id)
-    let e = elements.find(e => e.id === el.id)
-    if (key === 'toDelete') {
-      if (value) {
-        e.toDelete = true
+    let e
+
+    if (el) {
+      e = elements.find(e => e.id === el.id)
+
+      if (key === 'name'){
+        let start = 0
+        let end = 0
+        let ref = this.myRefs[`${el.id}_${key}`]
+  
+        if (ref && ref.input) {
+          start = ref.input.selectionStart
+          end = ref.input.selectionEnd
+        }
+  
+        if (value) {
+          e[key] = value
+          delete e[`${key}Error`]
+        }
+        else {
+          //blank value while typing.
+          e[key] = ''
+        }
+
+        await this.setState({f5elements: elements})
+        ref = this.myRefs[`${el.id}_${key}`]
+  
+        if (ref && ref.input) {
+          ref.input.selectionStart = start
+          ref.input.selectionEnd = end
+        }
+  
+        ref.focus()
       }
-      else {
-        delete e.toDelete
+
+      if (key === 'address'){
+        let start = 0
+        let end = 0
+        let ref = this.myRefs[`${el.id}_${key}`]
+  
+        if (ref && ref.input) {
+          start = ref.input.selectionStart
+          end = ref.input.selectionEnd
+        }
+  
+        if (value) {
+          e[key] = value
+          delete e[`${key}Error`]
+        }
+        else {
+          //blank value while typing.
+          e[key] = ''
+        }
+
+        await this.setState({f5elements: elements})
+        ref = this.myRefs[`${el.id}_${key}`]
+  
+        if (ref && ref.input) {
+          ref.input.selectionStart = start
+          ref.input.selectionEnd = end
+        }
+  
+        ref.focus()
+      }
+
+      if (key === 'routeDomain') {
+        value = value.toString()
+        if (value) {
+          e[key] = value
+          delete e[`${key}Error`]
+        }
+        else {
+          //blank value while typing.
+          e[key] = ''
+        }
+        await this.setState({f5elements: elements})
+      }
+
+      if (key === 'session') {
+        value = value.toString()
+        if (value) {
+          e[key] = value
+          delete e[`${key}Error`]
+        }
+        else {
+          //blank value while typing.
+          e[key] = ''
+        }
+        await this.setState({f5elements: elements})
+      }
+
+      if (key === 'state') {
+        value = value.toString()
+        if (value) {
+          e[key] = value
+          delete e[`${key}Error`]
+        }
+        else {
+          //blank value while typing.
+          e[key] = ''
+        }
+        await this.setState({f5elements: elements})
+      }
+  
+      if (key === 'toDelete') {
+        if (value) {
+          e.toDelete = true
+        }
+        else {
+          delete e.toDelete
+        }
+        await this.setState({f5elements: elements})
       }
     }
-    await this.setState({f5elements: elements})
+    
   }
-  
+
 
   render() {
-    console.log('f5elements', this.state.f5elements)
 
     let randomKey = () => {
       return Math.random().toString()
     }
 
-    let createElement = (element, key, choices, obj, action, event) => {
-      switch (element) {
-        case 'input':
+    let createElement = (element, key, choices, obj, action) => {
+      if (element === 'input') {
+        if (key === 'name') {
           return (
             <Input
+              value={obj[key]}
               style=
-              {this.state.errors[`${key}Error`] ?
-                {borderColor: 'red'}
-              :
-                {}
-              }
-              defaultValue={obj ? obj[key] : this.state.request ? this.state.request[key] : ''}
-              onChange={event => this.set(event.target.value, key)}
-              onPressEnter={() => this.validation(action)}
-            />
+                {obj[`${key}Error`] ?
+                  {borderColor: 'red', width: 200}
+                :
+                  {width: 200}
+                }
+              ref={ref => this.myRefs[`${obj.id}_${key}`] = ref}
+              onChange={event => this.set(key, event.target.value, obj)}
+            />          
           )
-          break;
-        
-        case 'checkbox':
+        }
+        else if (key === 'address') {
           return (
-            <Checkbox 
-              checked={obj.toDelete}
-              //onChange={e => console.log(action)}
-              onChange={e => this.set(action, e.target.checked, obj)}
-            />
+            <Input
+              value={obj[key]}
+              style=
+                {obj[`${key}Error`] ?
+                  {borderColor: 'red', width: 200}
+                :
+                  {width: 200}
+                }
+              ref={ref => this.myRefs[`${obj.id}_${key}`] = ref}
+              onChange={event => this.set(key, event.target.value, obj)}
+            />          
           )
-        
-        default:
+        }
 
       }
 
+      else if (element === 'select') {
+        if (key === 'routeDomain') {
+          return (
+            <Select
+              value={obj.routeDomain}
+              showSearch
+              style={{width: 150}}
+              optionFilterProp="children"
+              filterOption={(input, option) =>
+                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+              }
+              filterSort={(optionA, optionB) =>
+                optionA.children.toLowerCase().localeCompare(optionB.children.toLowerCase())
+              }
+              onSelect={event => this.set('routeDomain', event, obj)}
+            >
+              <React.Fragment>
+                {choices.map((rd, i) => {
+                  return (
+                    <Select.Option key={i} value={rd.id}>{rd.name}</Select.Option>
+                  )
+                })
+                }
+              </React.Fragment>
+            </Select>
+          )
+        }
+
+        else if (key === 'session') {
+          return (
+            <Select
+              value={obj.session}
+              showSearch
+              style={{width: 150}}
+              optionFilterProp="children"
+              filterOption={(input, option) =>
+                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+              }
+              filterSort={(optionA, optionB) =>
+                optionA.children.toLowerCase().localeCompare(optionB.children.toLowerCase())
+              }
+              onSelect={event => this.set('session', event, obj)}
+            >
+              <React.Fragment>
+                {choices.map((session, i) => {
+                  return (
+                    <Select.Option key={i} value={session}>{session}</Select.Option>
+                  )
+                })
+                }
+              </React.Fragment>
+            </Select>
+          )
+        }
+
+        else if (key === 'state') {
+          return (
+            <Select
+              value={obj.state}
+              showSearch
+              style={{width: 150}}
+              optionFilterProp="children"
+              filterOption={(input, option) =>
+                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+              }
+              filterSort={(optionA, optionB) =>
+                optionA.children.toLowerCase().localeCompare(optionB.children.toLowerCase())
+              }
+              onSelect={event => this.set('state', event, obj)}
+            >
+              <React.Fragment>
+                {choices.map((state, i) => {
+                  return (
+                    <Select.Option key={i} value={state}>{state}</Select.Option>
+                  )
+                })
+                }
+              </React.Fragment>
+            </Select>
+          )
+        }
+
+      }
+
+      else if (element === 'checkbox'){
+        return (
+          <Checkbox 
+            checked={obj.toDelete}
+            onChange={event => this.set(action, event.target.checked, obj)}
+          />
+        )
+      }
+
+      else if (element === 'button'){
+        if (action === 'elementRemove') {
+          return (
+            <Button
+              type='danger'
+              onClick={() => this.elementRemove(obj, this.state.f5elements)}
+            >
+              -
+            </Button>
+          )
+        }
+        else if (action === 'commit') {
+          return (
+            <Button
+              type="primary"
+              style={{float: 'right', marginRight: 5, marginBottom: 15}}
+              //onClick={() => this.validation()}
+              onClick={() => console.log('commit!!!')}
+            >
+              Commit
+            </Button>
+          )
+        }
+      }
     }
 
     let returnCol = () => {
       if (this.props.f5elements === 'nodes') {
         return nodesColumns
+      }
+    }
+
+    let rd = (address) => {
+      let val = address.split('%')
+      if (val.length > 1) {
+        return val[1]
+      }
+      else {
+        return null
       }
     }
 
@@ -257,10 +523,9 @@ class F5Elements extends React.Component {
         align: 'center',
         dataIndex: 'loading',
         key: 'loading',
-        render: (name, obj)  => (
+        render: (val, obj)  => (
           <Space size="small">
             {obj.loading ? <Spin indicator={elementLoadIcon} style={{margin: '10% 10%'}}/> : null }
-            {/*to do: createElement()*/} 
           </Space>
         ),
       },
@@ -277,6 +542,12 @@ class F5Elements extends React.Component {
         dataIndex: 'name',
         key: 'name',
         ...this.getColumnSearchProps('name'),
+        render: (val, obj)  => (
+          obj.existent ?
+            val
+          :
+            createElement('input', 'name', '', obj, '')
+        )
       },
       {
         title: 'Address',
@@ -284,20 +555,51 @@ class F5Elements extends React.Component {
         dataIndex: 'address',
         key: 'address',
         ...this.getColumnSearchProps('address'),
+        render: (val, obj)  => (
+          obj.existent ?
+            val
+          :
+            createElement('input', 'address', '', obj, '')
+        )
+      },
+      {
+        title: 'Route domain (optional)',
+        align: 'center',
+        dataIndex: 'routeDomain',
+        key: 'routeDomain',
+        ...this.getColumnSearchProps('routeDomain'),
+        render: (val, obj)  => (
+          obj.existent ?
+            rd(obj.address)
+          :
+            createElement('select', 'routeDomain', this.state.routeDomains, obj, '')
+        )
       },
       {
         title: 'Session',
         align: 'center',
         dataIndex: 'session',
         key: 'session',
-       ...this.getColumnSearchProps('session'),
+        ...this.getColumnSearchProps('session'),
+        render: (val, obj)  => (
+          obj.existent ?
+            val
+          :
+            createElement('select', 'session', this.state.nodeSessions, obj, '')
+        )
       },
       {
-        title: 'Status',
+        title: 'State',
         align: 'center',
         dataIndex: 'state',
         key: 'state',
-       ...this.getColumnSearchProps('state'),
+        ...this.getColumnSearchProps('state'),
+        render: (val, obj)  => (
+          obj.existent ?
+            val
+          :
+            createElement('select', 'state', this.state.nodeStates, obj, '')
+        )
       },
       {
         title: 'Monitor',
@@ -311,18 +613,12 @@ class F5Elements extends React.Component {
         align: 'center',
         dataIndex: 'delete',
         key: 'delete',
-        render: (name, obj)  => (
+        render: (val, obj)  => (
           <Space size="small">
-            {/*to do: createElement()*/} 
             { obj.existent ? 
               createElement('checkbox', 'toDelete', '', obj, 'toDelete')
             :
-              <Button
-                type='danger'
-                onClick={(e) => this.elementRemove(obj, this.state.f5elements)}
-              >
-                -
-              </Button>
+              createElement('button', 'elementRemove', '', obj, 'elementRemove')
             }
           </Space>
         ),
@@ -370,17 +666,13 @@ class F5Elements extends React.Component {
               pagination={{ pageSize: 10 }}
             />
             <br/>
-            {/*to do: createElement()*/} 
-            <Button
-              type="primary"
-              style={{float: 'right', marginRight: 5, marginBottom: 15}}
-              //onClick={() => this.validation()}
-            >
-              Commit
-            </Button>
+
+            {createElement('button', '', '', '', 'commit')}
+
           </React.Fragment>
         }
 
+        { this.props.routeDomainsError ? <Error object={this.props.f5elements} error={[this.props.routeDomainsError]} visible={true} type={'routeDomainsError'} /> : null }
         { this.props.nodesError ? <Error object={this.props.f5elements} error={[this.props.nodesError]} visible={true} type={'nodesError'} /> : null }
 
       </React.Fragment>
@@ -396,6 +688,7 @@ asset: state.f5.asset,
 partition: state.f5.partition,
 
 nodesError: state.f5.nodesError,
+routeDomainsError: state.f5.routeDomainsError,
 }))(F5Elements);
   
   
