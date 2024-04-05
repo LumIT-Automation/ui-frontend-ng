8c23728e28d9712556f830cedf01b3a1e7fa78ea
mik <m.sartori@lumit.it>
2024-04-05 10:29:11 +0200

    debian12

--- CONTAINER-DEBIAN-PKG/usr/lib/ui/bootstrap.sh
+++ CONTAINER-DEBIAN-PKG/usr/lib/ui/bootstrap.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 apt update
+apt install -y apt-utils ca-certificates gnupg
 apt install -y procps net-tools dnsutils wget curl nano
 
+mv /hashicorp-archive-keyring.gpg /etc/apt/trusted.gpg.d/
+mv /hashicorp.list /etc/apt/sources.list.d/
+￼
+apt update
 apt install -y /*.deb
 rm -f /*.deb
 
