46070105f2c279c4215fd86d8538e6a06ca70cf3
Lorenzo <l.pellegrin@lumit.it>
2024-06-03 15:18:24 +0200

    refactor function: Concerto

--- src/Concerto.js
+++ src/Concerto.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import {connect} from "react-redux"
-import { Component } from "react"
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import './App.css'
 import 'antd/dist/antd.css'
@@ -28,106 +27,82 @@ import Triggers from './concerto/triggers/triggers'
 
 import Configurations from './concerto/configurations'
 
-
-
-
-
 const { Content } = Layout;
 
 
-class Concerto extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+function Concerto(props) {
 
-  componentDidMount() {
-  }
-
-  shouldComponentUpdate(newProps, newState) {
-    return true;
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-  }
-
-  componentWillUnmount() {
-  }
-
-  isAuthorized = (authorizations, vendor, key) => {
+  const isAuthorized = (authorizations, vendor, key) => {
     let author = new Authorizators()
     return author.isAuthorized(authorizations, vendor, key)
   }
-
-  render() {
     
-    return (
-      <Layout style={{overflow: 'initial'}}>
-        <HeaderCustom/>
-        <BrowserRouter>
-          <Layout>
-            <CustomSider/>
-            <Layout style={{padding: '0 24px 24px'}}>
-              <Content
-                className="site-layout-background"
-                style={{
-                  margin: '3vh 0vw 0vh 0vw',
-                  minHeight: '100vh',
-                }}
-              >
-                <Switch>
+  return (
+    <Layout style={{overflow: 'initial'}}>
+      <HeaderCustom/>
+      <BrowserRouter>
+        <Layout>
+          <CustomSider/>
+          <Layout style={{padding: '0 24px 24px'}}>
+            <Content
+              className="site-layout-background"
+              style={{
+                margin: '3vh 0vw 0vh 0vw',
+                minHeight: '100vh',
+              }}
+            >
+              <Switch>
 
-                  <Route exact path='/historys/' component={Historys}/>
+                <Route exact path='/historys/' component={Historys}/>
 
-                  { this.isAuthorized(this.props.authorizations, 'infoblox') ?
-                    <Route path='/infoblox/' component={Infoblox}/>
-                  :
-                    null
-                  }
+                { isAuthorized(props.authorizations, 'infoblox') ?
+                  <Route path='/infoblox/' component={Infoblox}/>
+                :
+                  null
+                }
 
-                  { this.isAuthorized(this.props.authorizations, 'checkpoint') ?
-                    <Route path='/checkpoint/' component={Checkpoint}/>
-                  :
-                    null
-                  }
+                { isAuthorized(props.authorizations, 'checkpoint') ?
+                  <Route path='/checkpoint/' component={Checkpoint}/>
+                :
+                  null
+                }
 
-                  { this.isAuthorized(this.props.authorizations, 'f5') ?
-                    <Route path='/f5/' component={F5}/>
-                  :
-                    null
-                  }
-                  { this.isAuthorized(this.props.authorizations, 'f5') ?
-                    <Route path='/certificatesandkeys/' component={CertificatesAndKeys}/>
-                  :
-                    null
-                  }
+                { isAuthorized(props.authorizations, 'f5') ?
+                  <Route path='/f5/' component={F5}/>
+                :
+                  null
+                }
+                { isAuthorized(props.authorizations, 'f5') ?
+                  <Route path='/certificatesandkeys/' component={CertificatesAndKeys}/>
+                :
+                  null
+                }
 
-                  <Route path='/services/' component={Service}/>
-                  <Route path='/workflows/' component={Workflow}/>
-                  <Route path='/assets/' component={Assets}/>
-                  <Route path='/permissions/' component={Permissions}/>
-                  <Route path='/triggers/' component={Triggers}/>
-                  <Route path='/configurations/' component={Configurations}/>
+                <Route path='/services/' component={Service}/>
+                <Route path='/workflows/' component={Workflow}/>
+                <Route path='/assets/' component={Assets}/>
+                <Route path='/permissions/' component={Permissions}/>
+                <Route path='/triggers/' component={Triggers}/>
+                <Route path='/configurations/' component={Configurations}/>
 
-                </Switch>
-              </Content>
-            </Layout>
+              </Switch>
+            </Content>
           </Layout>
-        </BrowserRouter>
+        </Layout>
+      </BrowserRouter>
 
-        { 
-          (this.props.error && 
-            this.props.error.errorType === 'authorizationsError') ? 
-            <Error error={[this.props.error]} visible={true}/> 
-          : 
-            null        
-        }
+      { 
+        (props.error && 
+          props.error.errorType === 'authorizationsError') ? 
+          <Error error={[props.error]} visible={true}/> 
+        : 
+          null        
+      }
 
-      </Layout>
-    )
-  }
+    </Layout>
+  )
+  
 }
 
 
