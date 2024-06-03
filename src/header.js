47ce4d3fa44c24afed1f556a1854f402701e3459
Lorenzo <l.pellegrin@lumit.it>
2024-06-03 15:13:35 +0200

    refactor function: header

--- src/header.js
+++ src/header.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React from 'react';
 import {connect} from "react-redux"
-import { Component, } from "react"
 
 import './App.css'
 import 'antd/dist/antd.css'
@@ -15,28 +14,9 @@ const { Header } = Layout;
 
 
 
-class HeaderCustom extends Component {
+function HeaderCustom(props) {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
-  componentDidMount() {
-  }
-
-  shouldComponentUpdate(newProps, newState) {
-    return true;
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-  }
-
-  componentWillUnmount() {
-  }
-
-  localStorageRemove = async () => {
+  const localStorageRemove = async () => {
     try {
       localStorage.removeItem('token');
       localStorage.removeItem('username');
@@ -46,100 +26,97 @@ class HeaderCustom extends Component {
     }
   }
 
-  logout = async () => {
-    await this.localStorageRemove()
-    await this.props.dispatch( logout() )
+  const logoutHandler = async () => {
+    await localStorageRemove()
+    await props.dispatch(logout())
     document.location.href = '/'
   }
 
 
-  render() {
-    const HeaderColor = () => {
-      if (this.props.headerColor) {
-        return this.props.headerColor
-      }
+  const HeaderColor = () => {
+    if (props.headerColor) {
+      return props.headerColor
     }
-
-    const Banner = ({ data }) => {
-      return <img
-        src={`data:${data}`}
-        height={50}
-        width={'350px'}
-        style={{marginBottom: '2px'}}
-        alt='Banner'
-      />
-    }
-
-    const Logo = ({ data }) => {
-      return <img
-        src={`data:${data}`}
-        height={'64px'}
-        width={'64px'}
-        alt='Logo'
-      />
-    }
-
-    const menu = (
-      <Menu>
-        <Menu.Item key="logout" onClick={async () => await this.logout()}>Logout</Menu.Item>
-      </Menu>
-    )
-
-
-    return (
-      <Header className="header" style={{padding: '0 10px', backgroundColor: HeaderColor()}}>
-        <Row>
-          { this.props.banner ?
-            <React.Fragment>
-              <Col span={4} style={{justifyContent: 'center'}}>
-                <Banner data={this.props.banner}/>
-              </Col>
-              <Col span={15} style={{justifyContent: 'center'}}>
-              </Col>
-            </React.Fragment>
-          :
-            <React.Fragment>
-              {this.props.logoWhite ?
-                <React.Fragment>
-                  <Col span={1} style={{display: 'flex', justifyContent: 'center'}}>
-                    <Logo data={this.props.logoWhite}/>
-                  </Col>
-                  <Col span={18} style={{display: 'flex', justifyContent: 'center'}}>
-                  </Col>
-                </React.Fragment>
-              :
-                <React.Fragment>
-                  <Col span={4}>
-                  </Col>
-                  <Col span={15} style={{display: 'flex', justifyContent: 'center'}}>
-                  </Col>
-                </React.Fragment>
-              }
-            </React.Fragment>
-          }
-
-
-          { (this.props.username && this.props.token) ?
-            <Col span={5} style={{paddingRight: '10px'}}>
-              <div>
-                <Dropdown overlay={menu} trigger={['click']}>
-                  <Avatar
-                    style={{float: "right", marginTop: '15px'}}
-                    icon={<UserOutlined/>}
-                    //onClick={() => this.logout()}
-                  >
-                  </Avatar>
-                </Dropdown>
-                <p style={{float: "right", marginRight: '15px', color: 'white'}}>{this.props.username}</p>
-              </div>
-            </Col>
-          :
-            null
-          }
-        </Row>
-      </Header>
-    )
   }
+
+  const Banner = ({ data }) => {
+    return <img
+      src={`data:${data}`}
+      height={50}
+      width={'350px'}
+      style={{marginBottom: '2px'}}
+      alt='Banner'
+    />
+  }
+
+  const Logo = ({ data }) => {
+    return <img
+      src={`data:${data}`}
+      height={'64px'}
+      width={'64px'}
+      alt='Logo'
+    />
+  }
+
+  const menu = (
+    <Menu>
+      <Menu.Item key="logout" onClick={async () => await logoutHandler()}>Logout</Menu.Item>
+    </Menu>
+  )
+
+
+  return (
+    <Header className="header" style={{padding: '0 10px', backgroundColor: HeaderColor()}}>
+      <Row>
+        { props.banner ?
+          <React.Fragment>
+            <Col span={4} style={{justifyContent: 'center'}}>
+              <Banner data={props.banner}/>
+            </Col>
+            <Col span={15} style={{justifyContent: 'center'}}>
+            </Col>
+          </React.Fragment>
+        :
+          <React.Fragment>
+            {props.logoWhite ?
+              <React.Fragment>
+                <Col span={1} style={{display: 'flex', justifyContent: 'center'}}>
+                  <Logo data={props.logoWhite}/>
+                </Col>
+                <Col span={18} style={{display: 'flex', justifyContent: 'center'}}>
+                </Col>
+              </React.Fragment>
+            :
+              <React.Fragment>
+                <Col span={4}>
+                </Col>
+                <Col span={15} style={{display: 'flex', justifyContent: 'center'}}>
+                </Col>
+              </React.Fragment>
+            }
+          </React.Fragment>
+        }
+
+
+        { (props.username && props.token) ?
+          <Col span={5} style={{paddingRight: '10px'}}>
+            <div>
+              <Dropdown overlay={menu} trigger={['click']}>
+                <Avatar
+                  style={{float: "right", marginTop: '15px'}}
+                  icon={<UserOutlined/>}
+                >
+                </Avatar>
+              </Dropdown>
+              <p style={{float: "right", marginRight: '15px', color: 'white'}}>{props.username}</p>
+            </div>
+          </Col>
+        :
+          null
+        }
+      </Row>
+    </Header>
+  )
 }
 
 
@@ -149,5 +126,4 @@ export default connect((state) => ({
   logoWhite: state.authentication.logoWhite,
   banner: state.authentication.banner,
   headerColor: state.authentication.headerColor
-
 }))(HeaderCustom);
