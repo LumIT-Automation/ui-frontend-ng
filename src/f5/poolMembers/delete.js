d7a68948b25dcd2ce3a43d4643f3a74ef408eb6d
Lorenzo <l.pellegrin@lumit.it>
2024-11-17 17:01:54 +0100

    refactor: f5 poolmemebers delete

--- src/f5/poolMembers/delete.js
+++ src/f5/poolMembers/delete.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 import { connect } from 'react-redux'
 import 'antd/dist/antd.css'
+
 import Rest from '../../_helpers/Rest'
 import Error from '../../concerto/error'
 
@@ -19,40 +20,26 @@ const spinIcon = <LoadingOutlined style={{ fontSize: 50 }} spin />
 const deleteIcon = <DeleteOutlined style={{color: 'white' }}  />
 
 
-class Delete extends React.Component {
+function Delete(props) {
+  let [visible, setVisible] = useState(false);
+  let [loading, setLoading] = useState(false);
+  let [response, setResponse] = useState(false);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: false,
-      error: null,
-    };
-  }
+  useEffect(() => {
+    if (visible && response) {
+      setTimeout( () => setResponse(false), 2000)
+      setTimeout( () => props.dispatch(poolMembersFetch(true)), 2030)
+      setTimeout( () => closeModal(), 2050)
+    }
+  }, [visible, response]);
 
-  componentDidMount() {
-  }
-
-  shouldComponentUpdate(newProps, newState) {
-    return true;
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-  }
-
-  componentWillUnmount() {
-  }
-
-  details = () => {
-    this.setState({visible: true})
-  }
-
-
-  poolMemberDelete = async pool => {
-    this.setState({loading: true})
+  let poolMemberDelete = async pool => {
+    setLoading(true)
     let rest = new Rest(
       "DELETE",
       resp => {
-        this.setState({loading: false, response: true}, () => this.response())
+        setLoading(false)
+        setResponse(true)
       },
       error => {
         error = Object.assign(error, {
@@ -60,60 +47,49 @@ class Delete extends React.Component {
           vendor: 'f5',
           errorType: 'poolMemberDeleteError'
         })
-        this.props.dispatch(err(error))
-        this.setState({loading: false, response: false})
+        props.dispatch(err(error))
+        setLoading(false)
       }
     )
-    await rest.doXHR(`f5/${this.props.asset.id}/${this.props.partition}/pool/${this.props.poolName}/member/${this.props.obj.name}/`, this.props.token )
-  }
-
-  response = () => {
-    setTimeout( () => this.setState({ response: false }), 2000)
-    setTimeout( () => this.props.dispatch(poolMembersFetch(true)), 2030)
-    setTimeout( () => this.closeModal(), 2050)
+    await rest.doXHR(`f5/${props.asset.id}/${props.partition}/pool/${props.poolName}/member/${props.obj.name}/`, props.token )
   }
 
 
   //Close and Error
-  closeModal = () => {
-    this.setState({
-      visible: false,
-    })
+  let closeModal = () => {
+    setVisible(false)
   }
 
-
-  render() {
-
-    let errors = () => {
-      if (this.props.error && this.props.error.component === 'poolMembersDelete') {
-        return <Error error={[this.props.error]} visible={true}/> 
-      }
+  let errorsComponent = () => {
+    if (props.error && props.error.component === 'poolMembersDelete') {
+      return <Error error={[props.error]} visible={true}/> 
     }
+  }
 
-    return (
-      <Space direction='vertical'>
+  return (
+    <Space direction='vertical'>
 
-      <Button icon={deleteIcon} type='primary' danger onClick={() => this.details()} shape='round'/>
+    <Button icon={deleteIcon} type='primary' danger onClick={() => setVisible(true)} shape='round'/>
 
-        <Modal
-          title={<div><p style={{textAlign: 'center'}}>DELETE</p> <p style={{textAlign: 'center'}}>{this.props.obj.name}</p></div>}
-          centered
-          destroyOnClose={true}
-          visible={this.state.visible}
-          footer={''}
-          onOk={null}
-          onCancel={() => this.closeModal()}
-          width={750}
-          maskClosable={false}
-        >
-          { this.state.loading && <Spin indicator={spinIcon} style={{margin: '10% 48%'}}/> }
-          {!this.state.loading && this.state.response &&
+      <Modal
+        title={<div><p style={{textAlign: 'center'}}>DELETE</p> <p style={{textAlign: 'center'}}>{props.obj.name}</p></div>}
+        centered
+        destroyOnClose={true}
+        visible={visible}
+        footer={''}
+        onOk={null}
+        onCancel={() => closeModal()}
+        width={750}
+        maskClosable={false}
+      >
+        { loading && <Spin indicator={spinIcon} style={{margin: '10% 48%'}}/> }
+          {!loading && response &&
             <Result
                status="success"
                title="Deleted"
              />
           }
-          {!this.state.loading && !this.state.response &&
+          {!loading && !response &&
             <div>
               <Row>
                 <Col span={5} offset={10}>
@@ -123,12 +99,12 @@ class Delete extends React.Component {
               <br/>
               <Row>
                 <Col span={2} offset={10}>
-                  <Button type="primary" onClick={() => this.poolMemberDelete(this.props.obj)}>
+                  <Button type="primary" onClick={() => poolMemberDelete(props.obj)}>
                     YES
                   </Button>
                 </Col>
                 <Col span={2} offset={1}>
-                  <Button type="primary" onClick={() => this.closeModal()}>
+                  <Button type="primary" onClick={() => closeModal()}>
                     NO
                   </Button>
                 </Col>
@@ -136,14 +112,14 @@ class Delete extends React.Component {
             </div>
           }
 
-        </Modal>
+      </Modal>
 
-        {errors()}
+      {errorsComponent()}
 
-      </Space>
+    </Space>
 
-    )
-  }
+  )
+  
 }
 
 export default connect((state) => ({
